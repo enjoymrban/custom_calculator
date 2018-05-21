a16aff463d6c45b0035062a75a57ab72b5b47f32
Silvan Knecht <knecht.silvan@gmail.com>
2018-05-21 20:25:20 +0200

    aStart

--- js/javascript.js
+++ js/javascript.js
@@ -1,174 +1,70 @@
-var secondNumber = false;
-var calFinish = false;
+var numbers = [];
 var kindofcalculation = "";
-
+var current = "";
 
 function pressButtonNumber(number) {
 
-    if (calFinish){
+    document.getElementById("numberField").innerHTML += number;
 
-
-    }else{
-
-
-
-    if (secondNumber) {
-
-        document.getElementById("typeFieldNumbersOne").style.display = "none";
-
-        var typeField = document.getElementById("typeFieldNumbersTwo").innerHTML;
-        if (typeField == "0") {
-
-            document.getElementById("typeFieldNumbersTwo").innerHTML = number;
-        } else {
-
-
-            document.getElementById("typeFieldNumbersTwo").innerHTML = typeField + number;
-
-        }
-
-    } else {
-
-        var typeField = document.getElementById("typeFieldNumbersOne").innerHTML;
-        if (typeField == "0") {
-
-            document.getElementById("typeFieldNumbersOne").innerHTML = number;
-        } else {
-
-
-            document.getElementById("typeFieldNumbersOne").innerHTML = typeField + number;
-
-        }
-    }
-}
 
 }
 
-function setPoint(){
 
+function empty() {
 
-
-    if(calFinish){}else{
-
-        var typeField = document.getElementById("typeFieldNumbersTwo").innerHTML;
-
-
-        if(secondNumber){
-
-            if(typeField ==""){
-
-                document.getElementById("typeFieldNumbersTwo").innerHTML = "0.";
-
-
-
-            }else{
-
-                document.getElementById("typeFieldNumbersTwo").innerHTML = typeField +".";
-
-            }
-
-
-
-        }else{
-
-            var typeField = document.getElementById("typeFieldNumbersOne").innerHTML;
-
-            if(typeField == ""){
-
-                document.getElementById("typeFieldNumbersOne").innerHTML = "0.";
-
-
-
-            }else{
-
-                document.getElementById("typeFieldNumbersOne").innerHTML =  typeField +".";
-
-            }
-
-
-
-
-
-        }
-
-
-
-    }
-}
-
-function empty(){
-
-
-    document.getElementById("typeFieldNumbersOne").innerHTML = "";
-    document.getElementById("typeFieldNumbersTwo").innerHTML = "";
-    secondNumber = false;
-    calFinish = false;
-    kindofcalculation ="";
-    document.getElementById("typeFieldNumbersOne").style.display = "inline";
-
-
+    numbers = [];
+    document.getElementById("numberField").innerHTML = "0";
+    kindofcalculation = "";
 
 }
 
 function specialKeys(key) {
 
-    if(key == 5){
+    /*If key === 5 result is beeing calculated, else next number is added*/
+    if (key === 5) {
 
-        var firstField = parseInt(document.getElementById("typeFieldNumbersOne").innerHTML);
-        var secondField = parseInt(document.getElementById("typeFieldNumbersTwo").innerHTML);
-        document.getElementById("typeFieldNumbersOne").innerHTML ="";
-        document.getElementById("typeFieldNumbersTwo").innerHTML ="";
-        secondNumber = false;
-        calFinish = true;
 
-        switch (kindofcalculation){
+        switch (kindofcalculation) {
 
+            /*devide*/
             case 1:
+                current = document.getElementById("numberField").innerHTML;
+                document.getElementById("numberField").innerHTML = numbers[numbers.length - 1] / current;
 
-
-                document.getElementById("typeFieldNumbersOne").innerHTML = (firstField/secondField).toString();
-                document.getElementById("typeFieldNumbersOne").style.display = "inline";
-                kindofcalculation ="";
                 break;
 
+            /*multiply*/
             case 2:
 
-                document.getElementById("typeFieldNumbersOne").innerHTML = (firstField*secondField).toString();
-                document.getElementById("typeFieldNumbersOne").style.display = "inline";
-                kindofcalculation ="";
+
                 break;
 
+            /*minus*/
             case 3:
 
-                document.getElementById("typeFieldNumbersOne").innerHTML = (firstField-secondField).toString();
-                document.getElementById("typeFieldNumbersOne").style.display = "inline";
-                kindofcalculation ="";
+
                 break;
 
+            /*plus*/
             case 6:
 
-                document.getElementById("typeFieldNumbersOne").innerHTML = (firstField+secondField).toString();
-                document.getElementById("typeFieldNumbersOne").style.display = "inline";
-                kindofcalculation ="";
+
                 break;
 
 
         }
 
 
-
-
-    }else {
-
+    } else {
+        current = document.getElementById("numberField").innerHTML;
+        numbers.push(current);
         kindofcalculation = key;
-        secondNumber = true;
+        document.getElementById("numberField").innerHTML = "0";
 
     }
 
 
-
-
-    }
+}
 
 
 
