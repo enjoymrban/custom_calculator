a24ad9bd3509cd0675743c3b96d4584f0fe4f5dd
Silvan Knecht <knecht.silvan@gmail.com>
2018-05-22 22:53:27 +0200

    basic functions work

--- js/javascript.js
+++ js/javascript.js
@@ -4,7 +4,25 @@ var current = "";
 
 function pressButtonNumber(number) {
 
-    document.getElementById("numberField").innerHTML += number;
+    current = document.getElementById("numberField").innerHTML;
+    if (current === "undef") {
+        current = "";
+    }
+    current += number;
+    var testForNull = current.split('');
+    var i = 0;
+
+    /*Deleting leading zeros */
+    for (; i < testForNull.length; i++) {
+        if (testForNull[i] === "0" && testForNull[i+1] !== "." && testForNull.length > 1) {
+            testForNull.splice(0, 1);
+            i = 0;
+        } else {
+            break;
+        }
+    }
+
+    document.getElementById("numberField").innerHTML = testForNull.join("");
 
 
 }
@@ -18,42 +36,77 @@ function empty() {
 
 }
 
+function setPoint() {
+    current = document.getElementById("numberField").innerHTML;
+    if (checkForPoint(current)) {
+
+    } else {
+
+        if (current === "0") {
+            current = "0."
+        } else (
+            current += "."
+        );
+
+        document.getElementById("numberField").innerHTML = current;
+    }
+}
+
+function checkForPoint(current) {
+    var checkForPointArr = current.split("");
+    for (var i = 0; checkForPointArr.length > i; i++) {
+    if(checkForPointArr[i] === "."){
+        return true;
+    }
+    }
+
+}
+
 function specialKeys(key) {
+
 
     /*If key === 5 result is beeing calculated, else next number is added*/
     if (key === 5) {
-
+        var result = "";
+        current = document.getElementById("numberField").innerHTML;
+        numbers.push(current);
 
         switch (kindofcalculation) {
 
             /*devide*/
             case 1:
-                current = document.getElementById("numberField").innerHTML;
-                document.getElementById("numberField").innerHTML = numbers[numbers.length - 1] / current;
+                result = devide(numbers[numbers.length - 2], numbers[numbers.length - 1]);
 
                 break;
 
             /*multiply*/
             case 2:
-
+                result = multiply(numbers[numbers.length - 2], numbers[numbers.length - 1]);
 
                 break;
 
             /*minus*/
             case 3:
-
+                result = minus(numbers[numbers.length - 2], numbers[numbers.length - 1]);
 
                 break;
 
             /*plus*/
             case 6:
+                result = plus(numbers[numbers.length - 2], numbers[numbers.length - 1]);
 
+                break;
+
+            /*Default*/
+            default:
+
+                result = "ERROR";
 
                 break;
 
 
         }
-
+        document.getElementById("numberField").innerHTML = result.toFixed(5);
 
     } else {
         current = document.getElementById("numberField").innerHTML;
@@ -66,6 +119,29 @@ function specialKeys(key) {
 
 }
 
+function devide(a, b) {
+    if (b === "0") {
+        return "undef";
+    } else {
+        return a / b;
+    }
+
+}
+
+function multiply(a, b) {
+    return a * b;
+
+}
+
+function minus(a, b) {
+    return a - b;
+
+}
+
+function plus(a, b) {
+    return a + b;
+
+}
 
 
 
